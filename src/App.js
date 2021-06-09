1b3fdedb861e4a2006f34def628d74733179771e
allanboodhun <allan.boodhun@gmail.com>
2021-06-09 16:03:19 +0200

    initiate three

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Html } from "drei";
+
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello!</h1>
-    </div>
+      <Canvas colorManagement camera={{ position: [0, 0, 120], fov: 70 }}>
+        <Html fullscreen>
+          <div className='container'>
+            <h1 className='title'>Hello</h1>
+          </div>
+        </Html>
+      </Canvas>
   );
 }
 
